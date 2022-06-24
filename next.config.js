a165866bcea8778e7cad9d0bd6b678fdab4ad68a
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-06-24 19:09:28 +0200

    match next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+    reactRoot: true,
+  },
 };
